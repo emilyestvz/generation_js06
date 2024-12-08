dcd3c19891af22fe40ca3122b681a148a29d2501
Emily <emilys@genstudents.org>
2024-12-08 10:12:13 -0300

    Atualização sobre as Collections (update)

--- BLOCO_1/aula_05/colecoes/Array.ts
+++ BLOCO_1/aula_05/colecoes/Array.ts
@@ -4,13 +4,14 @@ import readlinesync = require('readline-sync');
 // variavel numero do tipo array, tipada como number 
 const numeros: Array<number> = new Array<number>();
 
-// Adicionamos os valores no final da coleção
+// PUSH: Adicionamos os valores no final da coleção
 numeros.push(1);
 numeros.push(7);
 numeros.push(10);
 numeros.push(5);
 numeros.push(2, 3);
 
+// O laço FOR OF foi criado para Arrays e Coleções
 // Listamos os dados que foram adicionados
 for(let numero of numeros){
     console.log(numero);
@@ -18,18 +19,21 @@ for(let numero of numeros){
 
 //numeros.push(readlinesync.questionFloat('Digite um numero: '));
 
-// Adiciona um ou mais elementos no inicio da fila
+// UNSHIFT: Adiciona um ou mais elementos no inicio da fila
 numeros.unshift(2, 4.5, 8, 9);
 
 console.table(numeros);
 
-// Verifica se um determinado elemento existe dentro do array
+// INCLUDES: Verifica se um determinado elemento existe dentro do array
 console.log(`\nExiste o numero 10 no Array?`, numeros.includes(5));
 
-// Verificamos qual é o indice de um elemento
+// INDEXOF: Verificamos qual é o indice de um elemento
 console.log(`\nQual o índice do numero 3 no Array?`, numeros.indexOf(3));
 
-// Remove um ou mais elementos do array
+// SPLICE: Remove um ou mais elementos do array
 numeros.splice(numeros.indexOf(10), 1);
 
 console.table(numeros);
+
+// ALTERAÇÃO: Alterando um elemento pelo indice
+numeros[numeros.indexOf(1)] = 11;
